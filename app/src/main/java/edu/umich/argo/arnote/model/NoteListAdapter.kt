773ebfb00a7eca28b7cdf6ee6b9fa010c6f963b6
ylyao <ylyao@umich.edu>
2022-03-17 16:34:23 -0400

    Import textbox

--- app/src/main/java/edu/umich/argo/arnote/model/NoteListAdapter.kt
+++ app/src/main/java/edu/umich/argo/arnote/model/NoteListAdapter.kt
@@ -8,6 +8,7 @@ import android.widget.ArrayAdapter
 import edu.umich.argo.arnote.R
 import edu.umich.argo.arnote.ar.PlaceNode
 import android.graphics.Color
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 
@@ -18,11 +19,15 @@ class NoteListAdapter(context: Context, users: MutableList<Place>) :
         val rowView = LayoutInflater.from(context).inflate(R.layout.listitem_note, parent, false)
 
         getItem(position)?.run {
-            val tView=rowView.findViewById<TextView>(R.id.tView)
-            val iView=rowView.findViewById<ImageView>(R.id.iView)
-            tView.text=name
+            val tView = rowView.findViewById<TextView>(R.id.tView)
+            val iView = rowView.findViewById<ImageView>(R.id.iView)
+            val share = rowView.findViewById<ImageButton>(R.id.imageButton)
+            tView.text = name
             iView.setImageResource(R.mipmap.pin_full_color)
             rowView.setBackgroundColor(Color.parseColor(if (position % 2 == 0) "#E0E0E0" else "#EEEEEE"))
+            share.setOnClickListener {
+
+            }
         }
         return rowView
     }
