9ecbbc462074fdd6f915d02934e1dc3d0de9f382
ylyao <ylyao@umich.edu>
2022-03-17 16:58:02 -0400

    Button click

--- app/src/main/java/edu/umich/argo/arnote/NoteActivity.kt
+++ app/src/main/java/edu/umich/argo/arnote/NoteActivity.kt
@@ -2,9 +2,13 @@ package edu.umich.argo.arnote
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.widget.ListView
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
+import androidx.cardview.widget.CardView
 import edu.umich.argo.arnote.ar.NoteStore
 import edu.umich.argo.arnote.model.NoteListAdapter
 import edu.umich.argo.arnote.ar.NoteStore.getNote
@@ -12,15 +16,21 @@ import edu.umich.argo.arnote.ar.NoteStore.getNote
 class NoteActivity : AppCompatActivity() {
     private lateinit var toolbar: Toolbar
     private lateinit var noteListView: ListView
+    private lateinit var importButton: View
+    private lateinit var cardView: CardView
+    private lateinit var childImport: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note)
         toolbar = findViewById(R.id.toolbar)
         noteListView= findViewById(R.id.noteListView)
+        importButton = findViewById(R.id.importButton)
+        cardView = findViewById(R.id.import_box)
+        childImport = findViewById(R.id.child_import)
         initToolbar()
         initNoteListView()
-
+        initImports()
     }
     private fun initNoteListView(){
         val notes=getNote()
@@ -35,6 +45,18 @@ class NoteActivity : AppCompatActivity() {
             finish()
         }
         toolbar.inflateMenu(R.menu.plainmenu)
+    }
 
+    private fun initImports() {
+        cardView.visibility = INVISIBLE
+        childImport.setOnClickListener {
+            importButton.visibility = VISIBLE
+            cardView.visibility = INVISIBLE
+        }
+    }
+
+    fun importNote(view: View?) {
+        view?.visibility = INVISIBLE
+        cardView.visibility = VISIBLE
     }
 }
