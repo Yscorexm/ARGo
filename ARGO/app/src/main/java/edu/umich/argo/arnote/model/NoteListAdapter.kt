ca7603f00dd2938db8407fef7199176ad128e2af
Scarlet <940012978@qq.com>
2022-04-07 20:50:25 -0400

    yxxyxxyxx

--- ARGO/app/src/main/java/edu/umich/argo/arnote/model/NoteListAdapter.kt
+++ ARGO/app/src/main/java/edu/umich/argo/arnote/model/NoteListAdapter.kt
@@ -8,6 +8,14 @@ import edu.umich.argo.arnote.R
 import android.graphics.Color
 import android.widget.*
 import edu.umich.argo.arnote.model.NoteStore.postNote
+import android.graphics.BitmapFactory
+
+import android.graphics.Bitmap
+import java.io.IOException
+import java.io.InputStream
+import java.net.MalformedURLException
+import java.net.URL
+
 
 class NoteListAdapter(context: Context, users: MutableList<Place>) :
     ArrayAdapter<Place?>(context, 0, users as List<Place?>) {
@@ -20,7 +28,20 @@ class NoteListAdapter(context: Context, users: MutableList<Place>) :
             val iView = rowView.findViewById<ImageView>(R.id.iView)
             val share = rowView.findViewById<ImageButton>(R.id.imageButton)
             tView.text = message
+//            imageUri?.let{
+//                try {
+//                    val bitmap =
+//                        BitmapFactory.decodeStream(URL(it).getContent() as InputStream)
+//                    iView.setImageBitmap(bitmap)
+//                } catch (e: MalformedURLException) {
+//                    e.printStackTrace()
+//                } catch (e: IOException) {
+//                    e.printStackTrace()
+//                }
+//            }?:run{
             iView.setImageResource(R.mipmap.pin_full_color)
+//            }
+
             rowView.setBackgroundColor(Color.parseColor(if (position % 2 == 0) "#E0E0E0" else "#EEEEEE"))
             share.setOnClickListener {
                 // once a note is shared, upload the note to backend
