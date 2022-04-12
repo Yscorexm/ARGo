2803996e073f4668d525a36c256b8ca6cea454f4
Scarlet1ssimo <940012978@qq.com>
2022-04-12 18:26:38 +0000

    MVP backend

--- backend/routing/urls.py
+++ backend/routing/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('getnote/', views.getnote, name='getnote'),
     path('postnoteplace/', views.postnoteplace, name='postnoteplace'),
-]
+    path('postnoteimage/', views.postnoteimage, name='postnoteimage'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
